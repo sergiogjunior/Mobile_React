1308cf5e4a5e144148ddc33e2698ff3d8b7926e3
sergiogjunior <sergiogjunior2007@gmail.com>
2025-04-17 11:40:01 -0300

    .

--- UC12_React_Native/banco_react/App.js
+++ UC12_React_Native/banco_react/App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state =
+    {
+
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.topo}>Banco R.N</Text>
+        <TextInput  style={styles.input} placeholder='Digite seu nome aqui...'>
+        </TextInput>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
+  },
+  topo:{
+    textAlign: 'center',
+    fontSize: 30,
+    color:'green',
+    marginTop: 30
+  },
+  input:{
+    padding:10,
+    height:50,
+    borderWidth:1,
+    margin:8,
+    borderRadius:15,
+    borderColor: 'black',
+    fontSize: 20,
+    backgroundColor:'#fff',
   },
 });
+export default App;
