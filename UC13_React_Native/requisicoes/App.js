ba27113c04972132e1cf4d021228ea8fcf40842b
sergiogjunior <sergiogjunior2007@gmail.com>
2025-06-05 11:40:47 -0300

    .

--- UC13_React_Native/requisicoes/App.js
+++ UC13_React_Native/requisicoes/App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import api from './src/services/api';
+
 export default function App() {
+
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(()=>{
+
+    async function getFilmes() {
+      const resposta = await api.get('r-api/?api=filmes');
+      setFilmes(resposta.data)
+      
+    }
+getFilmes();
+
+  },[]);
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text>Filmes</Text>
+      
     </View>
   );
 }
