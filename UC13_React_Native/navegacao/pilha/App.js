5ed5d0c2ca629ffb4aef387c4ddcba618ae052df
sergiogjunior <sergiogjunior2007@gmail.com>
2025-06-26 08:33:17 -0300

    Update App.js

--- UC13_React_Native/navegacao/pilha/App.js
+++ UC13_React_Native/navegacao/pilha/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContmainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from './pages/Home';
